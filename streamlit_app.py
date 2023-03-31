2d78849a627afd5f1c64c731831934965573b450
Rkonda23 <128413249+Rkonda23@users.noreply.github.com>
2023-03-31 16:27:18 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,19 +53,29 @@ def get_fruit_load_list():
     
 #Add button to load the fruit
 if streamlit.button('Get Fruit Load List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   # my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 # take the json version of the response and normalize it 
+#Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+            return 'Thanks for adding ', new_fruit
+ 
+add_my_fruit = streamlit.text_input('What fruit would you like to add ?')
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
 
 # output it the screen as a table
 
 #don't run anything past here while we troubleshooting 
 streamlit.stop()
 
-#Allow the end user to add a fruit to the list
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add','Jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
+
+#streamlit.write()
 #This will not work correctly, but just go with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
